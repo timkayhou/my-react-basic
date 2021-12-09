652f38d354c29f05557df89c1e2173f51f662f81
Expurgator <timkayhou@gmail.com>
2021-12-09 23:25:54 +0900

    ✨ feat: end game while win

--- src/index.js
+++ src/index.js
@@ -22,6 +22,9 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice()
+    if (calculateWinner(squares) || squares[i]) {
+      return
+    }
     squares[i] = this.state.xIsNext ? '×' : '〇'
     this.setState({
       squares: squares,
