06c3b0cdae319094057094f293d1b72f40f6a33e
Expurgator <timkayhou@gmail.com>
2021-12-09 23:21:02 +0900

    ✨ feat: add winner

--- src/index.js
+++ src/index.js
@@ -33,8 +33,12 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? '×' : '〇')
-
+    const winner = calculateWinner(this.state.squares)
+    let status
+    winner ?
+      status = 'Winner: ' + winner
+    :
+      status = 'Next player: ' + (this.state.xIsNext ? '×' : '〇')
     return (
       <div>
         <div className="status">{status}</div>
@@ -74,7 +78,25 @@ class Game extends React.Component {
   }
 }
 
-// ========================================
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 
 ReactDOM.render(
   <Game />,
