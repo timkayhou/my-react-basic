287429f3cc8bb5d7a35211480734e7ab56c5a824
Expurgator <timkayhou@gmail.com>
2021-12-09 21:52:11 +0900

    ✨ feat(console "click" when click square):

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
+      <button className="square" onClick={() => { console.log('click');} }>
         {this.props.value}
       </button>
     );
